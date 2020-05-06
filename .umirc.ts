5f8100dda71f7f7f13de66e738e80c13ce57cef0
hanyueqiang <webyueqiang@163.com>
2020-05-06 23:48:22 +0800

    :zap: .umirc config

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  title: '管理平台',
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  locale: {},
+  // 是否启用按需加载
+  // dynamicImport: {},
   // 设置 node_modules 目录下依赖文件的编译方式
   nodeModulesTransform: {
     type: 'none',
   },
-  hash: true,
-  history: {
-    type: 'memory',
+  targets: {
+    ie: 11,
+  },
+  theme: {
+    '@primary-color': '#1DA57A',
+  },
+  proxy: {
+    '/api': {
+      target: 'http://128.1.1.1:8010/',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
   },
 });
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
 
 export default () => {
   return (
     <div>
       <h1>users</h1>
+      <Button type="primary">你好</Button>
     </div>
   );
 };
